678b8c26efd54f0fecd4cd3b2c24626ca27690e8
18866805256 <3407085928@qq.com>
2023-01-07 13:43:45 +0800

    new file:   problem11.cpp

---
+++ problem11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int score = 0;
+    cin >> score;
+    if (score >= 90 && score <= 100)
+    {
+        cout << "Perfect" << endl;
+    }
+    system("pause");
+    return 0;
+}
