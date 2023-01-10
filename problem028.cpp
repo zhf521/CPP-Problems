320882ab8a0fdfe1cacd09b0c0b249ebf5f9df22
18866805256 <3407085928@qq.com>
2023-01-10 14:09:53 +0800

    new file:   problem028.cpp

---
+++ problem028.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                for (int k = j; k < n; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    system("pause");
+    return 0;
+}
